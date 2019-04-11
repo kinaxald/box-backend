1348a6e7f8a2d80497611989e6c4f8ec7ca259f2
Kinaxald <teteusfurtado@hotmail.com>
2019-04-11 11:29:26 -0300

    Base back-end

---
+++ src/server.js
@@ -0,0 +1,43 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const path =  require ('path');
+const cors =  require('cors');
+
+
+const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require ('socket.io')(server);
+
+io.on('connection', socket =>{
+
+    console.log('ta bombando');
+    socket.on('connectRoom', box =>{
+
+        socket.join(box);
+
+    });
+
+});
+mongoose.connect ('mongodb+srv://omni:omni@app-t9mk6.mongodb.net/omni?retryWrites=true',
+{
+    useNewUrlParser:true
+
+});
+
+app.use((req, res, next)=>{
+    req.io = io;
+
+    return next();
+});
+app.use (express.json());
+
+app.use(express.urlencoded({extended: true})); // Permite o envio de arquivos
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+server.listen (3333);
