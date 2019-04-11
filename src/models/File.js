1262646aef6225d8ca51022e69edea2e4bfdfbb9
Kinaxald <teteusfurtado@hotmail.com>
2019-04-11 11:54:49 -0300

    ...

--- src/models/File.js
+++ src/models/File.js
@@ -19,9 +19,9 @@ const File = new mongoose.Schema({
 
 File.virtual('url').get(function(){
 
-    const url = process.env.PORT || 'http://localhost:3333';
+    const URL = process.env.PORT || 'http://localhost:3333';
     
-    return `${url}/files/${encodeURIComponent(this.path)}`;
+    return `${URL}/files/${encodeURIComponent(this.path)}`;
 })
 
 module.exports = mongoose.model ('File', File);
